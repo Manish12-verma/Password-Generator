6dc5f9dd7737a7196f7657daa6387d0db45c405c
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-07-31 00:21:55 +0530

    added handleCheckBox functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
+import { useState } from "react";
+
 function App() {
+  
+  const [length,setLength] = useState(4);
+  const [checkboxData,setCheckboxData] = useState([
+      {title:"Include Uppercase", state: false},
+    {title:"Include Lowercase", state: false},
+    {title:"Include Numbers", state: false},
+    {title:"Include Symbols", state: false}
+  ]);
+  
+  const handleCheckBoxChange = (index) => {
+         const updatedCheckboxData = [...checkboxData];
+         updatedCheckboxData[index].state = !updatedCheckboxData[index].state;
+         setCheckboxData(updatedCheckboxData);
+  }
 
-
+  
   return (
     <div className="container">
-
       <div className="header">
-           <div className="title">jry8762$#</div>
-           <button className="copyBtn" onClick={()=>{}}>copy</button>
+        <div className="title">jry8762$#</div>
+        <button className="copyBtn" onClick={() => {}}>
+          copy
+        </button>
       </div>
+
+      <div className="charlength">
+        <span>
+          <label>Character Length</label>
+          <label>{length}</label>
+        </span>
+        <input type="range" min='4' max='20' onChange={(e)=>setLength(e.target.value)} value={length} 
+        
+        />
+      </div>
+      <div className="checkboxes">
+        {checkboxData.map((item, index) => (
+          <div className="checkbox" key={index}>
+            <input type="checkbox" checked={item.state} onChange={() => handleCheckBoxChange(index)} />
+            <label>{item.title}</label>
+          </div>
+        ))}
+      </div>
+      {/*strength*/}
+
+      <button className="generateBtn" onClick={()=>{
+
+      }}>Generate Password</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
