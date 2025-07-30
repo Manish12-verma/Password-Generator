6b442c6fe4b08d4b408da4f1b677c18cdfc8616a
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-07-30 11:45:05 +0530

    initialised repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+
+  return (
+    <div className="container">
+
+      <div className="header">
+           <div className="title">jry8762$#</div>
+           <button className="copyBtn" onClick={()=>{}}>copy</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
